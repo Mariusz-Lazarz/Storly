3236f345fae7d9c26b7e1094efb4401d3c82b527
Mariusz Łazarz <mariusz.lazarz97@gmail.com>
2023-10-03 12:30:38 +0200

    adding validation for inputing quantity manually

--- src/components/Store.js
+++ src/components/Store.js
@@ -4,53 +4,7 @@ import { getDatabase, ref, onValue, off } from "firebase/database";
 import { addToCart } from "../store/cartSlice";
 import { getAuth } from "firebase/auth";
 import { eCommerce } from "@piwikpro/react-piwik-pro";
-
-// Component to render each item
-function StoreItem({
-  item,
-  selectedQuantities,
-  handleQuantityChange,
-  handleAddToCart,
-}) {
-  return (
-    <div className="bg-white p-4 rounded shadow-lg">
-      <img
-        src={item.imageLink}
-        alt={item.title}
-        className="w-full h-48 object-cover rounded mb-4"
-      />
-      <h3 className="text-lg font-semibold mb-2">
-        {item.title} <span className="text-gray-600">${item.price}</span>
-      </h3>
-      <div className="flex items-center gap-4 mb-4">
-        <div className="flex-grow">
-          <label
-            htmlFor={`quantity-${item.id}`}
-            className="block text-sm font-medium text-gray-700"
-          >
-            Quantity
-          </label>
-          <input
-            type="number"
-            id={`quantity-${item.id}`}
-            name={`quantity-${item.id}`}
-            min="1"
-            max={item.quantity}
-            value={selectedQuantities[item.id] || 1}
-            onChange={(e) => handleQuantityChange(item.id, e.target.value)}
-            className="mt-1 block w-10 rounded-md border-gray-300 shadow-sm focus:border-light-pink focus:ring focus:ring-light-pink focus:ring-opacity-50"
-          />
-        </div>
-        <button
-          className="bg-light-pink text-white py-2 px-4 rounded-full"
-          onClick={() => handleAddToCart(item)}
-        >
-          Add to Cart
-        </button>
-      </div>
-    </div>
-  );
-}
+import StoreItem from "./StoreItem";
 
 // Main Store Component
 function Store() {
---
+++ src/components/StoreItem.js
@@ -0,0 +1,61 @@
+// Component to render each item
+function StoreItem({
+  item,
+  selectedQuantities,
+  handleQuantityChange,
+  handleAddToCart,
+}) {
+  return (
+    <div className="bg-white p-4 rounded shadow-lg">
+      <img
+        src={item.imageLink}
+        alt={item.title}
+        className="w-full h-48 object-cover rounded mb-4"
+      />
+      <h3 className="text-lg font-semibold mb-2">
+        {item.title} <span className="text-gray-600">${item.price}</span>
+      </h3>
+      <div className="flex items-center gap-4 mb-4">
+        <div className="flex-grow">
+          <label
+            htmlFor={`quantity-${item.id}`}
+            className="block text-sm font-medium text-gray-700"
+          >
+            Quantity
+          </label>
+          <input
+            type="number"
+            id={`quantity-${item.id}`}
+            name={`quantity-${item.id}`}
+            min="1"
+            max={item.quantity}
+            value={selectedQuantities[item.id] || 1}
+            onChange={(e) => {
+              // Ensure the entered value is within the valid range
+              let newValue = parseInt(e.target.value, 10);
+              if (isNaN(newValue)) {
+                newValue = 1; // Default to 1 if not a number
+              } else if (newValue < 1) {
+                newValue = 1; // Minimum allowed value is 1
+              } else if (newValue > item.quantity) {
+                newValue = item.quantity; // Maximum allowed value is the item's quantity
+              }
+
+              // Update the selectedQuantities state with the validated value
+              handleQuantityChange(item.id, newValue);
+            }}
+            className="mt-1 block w-12 rounded-md border-gray-300 shadow-sm focus:border-light-pink focus:ring focus:ring-light-pink focus:ring-opacity-50"
+          />
+        </div>
+        <button
+          className="bg-light-pink text-white py-2 px-4 rounded-full"
+          onClick={() => handleAddToCart(item)}
+        >
+          Add to Cart
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default StoreItem;
