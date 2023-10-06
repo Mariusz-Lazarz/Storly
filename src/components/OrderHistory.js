f720cf121be8f76617c0bba603075789a1674d97
Mariusz Łazarz <mariusz.lazarz97@gmail.com>
2023-10-06 11:26:50 +0200

    fixing manually inputing url for userPanel

--- src/components/OrderHistory.js
+++ src/components/OrderHistory.js
@@ -5,27 +5,36 @@ import { getDatabase, ref, onValue, off } from "firebase/database";
 import { getAuth } from "firebase/auth";
 
 function OrderHistory() {
-  const auth = getAuth();
-  const curUser = auth.currentUser;
   const [orders, setOrders] = useState([]);
-
-  console.log(orders);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    const auth = getAuth();
     const db = getDatabase();
-    const ordersRef = ref(db, `orders/${curUser.uid}`);
 
-    const handleData = (snapshot) => {
-      const data = snapshot.val();
-      const ordersArray = data
-        ? Object.keys(data).map((key) => ({ id: key, ...data[key] }))
-        : [];
-      setOrders(ordersArray);
-    };
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        const ordersRef = ref(db, `orders/${user.uid}`);
 
-    onValue(ordersRef, handleData);
+        const handleData = (snapshot) => {
+          const data = snapshot.val();
+          console.log(data);
+          const ordersArray = data
+            ? Object.keys(data).map((key) => ({ id: key, ...data[key] }))
+            : [];
+          setOrders(ordersArray);
+          setIsLoading(false);
+        };
+
+        onValue(ordersRef, handleData);
+        return () => {
+          off(ordersRef, "value", handleData);
+        };
+      }
+    });
+
     return () => {
-      off(ordersRef, "value", handleData);
+      unsubscribe();
     };
   }, []);
 
@@ -33,49 +42,57 @@ function OrderHistory() {
 
   return (
     <div className="p-2">
-      {orders.map((order) => (
-        <div key={order.id} className="border-l-4 border-green-500 p-4 mb-4">
-          <div
-            className="flex flex-col sm:flex-row justify-between cursor-pointer"
-            onClick={() =>
-              setExpandedOrderId((prevId) =>
-                prevId === order.id ? null : order.id
-              )
-            }
-          >
-            <span className="mb-2 sm:mb-0">ID: {order.id}</span>
-            <span className="mb-2 sm:mb-0">Date: {order.date}</span>
-            <span className="mb-2 sm:mb-0">Value: ${order.value}</span>
-            <span
-              className={`transform transition-transform ${
-                order.id === expandedOrderId ? "rotate-180" : ""
-              }`}
-            >
-              <FontAwesomeIcon
-                icon={faChevronDown}
-                className={order.id === expandedOrderId ? "fa-rotate-180" : ""}
-              />
-            </span>
-          </div>
-          {order.id === expandedOrderId && (
-            <div className="mt-4">
-              {order.items.map((item, index) => (
-                <div
-                  key={index}
-                  className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-4 p-2 border-b"
-                >
-                  <span>{item.item_title}</span>
-                  <span>ID: {item.item_id}</span>
-                  <span>Price: ${item.price}</span>
-                  <span>Category: {item.item_category}</span>
-                  <span>Brand: {item.item_brand}</span>
-                  <span>Quantity: {item.quantity}</span>
-                </div>
-              ))}
-            </div>
-          )}
+      {isLoading ? (
+        <div className="mt-2 flex justify-center items-center">
+          <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-blue-500"></div>
         </div>
-      ))}
+      ) : (
+        orders.map((order) => (
+          <div key={order.id} className="border-l-4 border-green-500 p-4 mb-4">
+            <div
+              className="flex flex-col sm:flex-row justify-between cursor-pointer"
+              onClick={() =>
+                setExpandedOrderId((prevId) =>
+                  prevId === order.id ? null : order.id
+                )
+              }
+            >
+              <span className="mb-2 sm:mb-0">ID: {order.id}</span>
+              <span className="mb-2 sm:mb-0">Date: {order.date}</span>
+              <span className="mb-2 sm:mb-0">Value: ${order.value}</span>
+              <span
+                className={`transform transition-transform ${
+                  order.id === expandedOrderId ? "rotate-180" : ""
+                }`}
+              >
+                <FontAwesomeIcon
+                  icon={faChevronDown}
+                  className={
+                    order.id === expandedOrderId ? "fa-rotate-180" : ""
+                  }
+                />
+              </span>
+            </div>
+            {order.id === expandedOrderId && (
+              <div className="mt-4">
+                {order.items.map((item, index) => (
+                  <div
+                    key={index}
+                    className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-4 p-2 border-b"
+                  >
+                    <span>{item.item_title}</span>
+                    <span>ID: {item.item_id}</span>
+                    <span>Price: ${item.price}</span>
+                    <span>Category: {item.item_category}</span>
+                    <span>Brand: {item.item_brand}</span>
+                    <span>Quantity: {item.quantity}</span>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        ))
+      )}
     </div>
   );
 }
