0faf0ecf610714dde5e3d9c36b6829140cafbdb9
Mariusz Łazarz <mariusz.lazarz97@gmail.com>
2023-10-09 11:01:30 +0200

    removing import

--- src/components/UserPanel/UserData.js
+++ src/components/UserPanel/UserData.js
@@ -1,4 +1,3 @@
-import ChangeEmail from "./ChangeEmail";
 import ChangePassword from "./ChangePassword";
 import DeleteAccount from "./DeleteAccount";
 
@@ -6,7 +5,6 @@ const UserData = () => {
   return (
     <div className="container mx-auto p-6">
       <h1 className="text-4xl font-bold mb-4">User Data</h1>
-      {/* <ChangeEmail /> */}
       <ChangePassword />
       <DeleteAccount />
     </div>
