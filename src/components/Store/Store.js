0bc4be58223c3860bf68339013fbd2a4eb362209
Mariusz Łazarz <mariusz.lazarz97@gmail.com>
2023-10-10 20:40:16 +0200

    styling store item

--- src/components/Store/Store.js
+++ src/components/Store/Store.js
@@ -106,7 +106,7 @@ const Store = () => {
       {isLoading ? (
         <LoadingSpinner></LoadingSpinner>
       ) : (
-        <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-7 gap-4">
+        <div className="grid grid-cols-3 md:grid-cols-7 lg:grid-cols-10 gap-4">
           {filteredItems.length > 0 ? (
             filteredItems.map((item) => (
               <StoreItem
--- src/components/Store/StoreItem.js
+++ src/components/Store/StoreItem.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
-import { Tooltip, TooltipProvider } from "react-tooltip";
+import { Tooltip } from "react-tooltip";
 
 const StoreItem = ({
   item,
@@ -12,80 +12,71 @@ const StoreItem = ({
 }) => {
   const isInCart = itemsInCart.includes(item.id);
 
+  const handleOnChange = (e) => {
+    let newValue = parseInt(e.target.value, 10);
+    if (isNaN(newValue)) {
+      newValue = 1;
+    } else if (newValue < 1) {
+      newValue = 1;
+    } else if (newValue > item.quantity) {
+      newValue = item.quantity;
+    }
+
+    handleQuantityChange(item.id, newValue);
+  };
+
   return (
-    <TooltipProvider>
-      <div className="bg-white p-2 rounded shadow-lg border relative">
-        <div className="h-24 flex items-center justify-center mb-4">
+    <div className="p-2 relative text-center">
+      <Link to={`/product/${item.id}`} state={item}>
+        <div className="flex items-center justify-center mb-2 h-24 w-24 mx-auto cursor-pointer">
           <img
             src={item.imageLink}
             alt={item.title}
-            className="object-cover rounded max-w-fit h-24"
+            className="object-cover w-full h-full"
           />
         </div>
-        <h3 className="font-semibold mb-2 text-base">
-          {item.title} <span className="text-orange-600">${item.price}</span>
-        </h3>
-        <div className="flex items-center gap-4 mb-1">
-          <div className="flex-grow">
-            <label
-              htmlFor={`quantity-${item.id}`}
-              className="block text-sm font-medium text-gray-700"
-            >
-              Quantity
-            </label>
-            <input
-              type="number"
-              id={`quantity-${item.id}`}
-              name={`quantity-${item.id}`}
-              min="1"
-              max={item.quantity}
-              value={selectedQuantities[item.id] || 1}
-              onChange={(e) => {
-                let newValue = parseInt(e.target.value, 10);
-                if (isNaN(newValue)) {
-                  newValue = 1;
-                } else if (newValue < 1) {
-                  newValue = 1;
-                } else if (newValue > item.quantity) {
-                  newValue = item.quantity;
-                }
-
-                handleQuantityChange(item.id, newValue);
-              }}
-              className="mt-1 block w-12 rounded-md border-gray-300 shadow-sm focus:border-light-pink focus:ring focus:ring-light-pink focus:ring-opacity-50"
-              disabled={isInCart}
-            />
-          </div>
-          <button
-            className={`py-1 px-2 rounded ${
-              isInCart ? "bg-gray-300 cursor-not-allowed" : "bg-light-pink"
-            }`}
-            onClick={() => handleAddToCart(item)}
+        <h3 className="font-semibold">{item.title}</h3>
+        <span className="text-orange-600">${item.price}</span>
+      </Link>
+      <div className="flex items-center m-1">
+        <div className="flex-grow">
+          <input
+            type="number"
+            id={`quantity-${item.id}`}
+            name={`quantity-${item.id}`}
+            min="1"
+            max={item.quantity}
+            value={selectedQuantities[item.id] || 1}
+            onChange={handleOnChange}
+            className="mt-1 block w-12 rounded-md border-gray-30 focus:ring-opacity-50 bg-transparent focus:outline-none"
             disabled={isInCart}
-            data-tooltip-id="my-tooltip"
-            data-tooltip-content={isInCart ? "Item already in cart" : null}
-          >
-            <FontAwesomeIcon
-              icon={faShoppingCart}
-              className={`text-white ${isInCart ? "text-gray-500" : ""}`}
-            />
-          </button>
-          <Tooltip
-            id="my-tooltip"
-            style={{
-              backgroundColor: "rgb(2,0,36)",
-              color: "white",
-              zIndex: "999",
-            }}
           />
         </div>
-        <div className="flex justify-center">
-          <Link to={`/product/${item.id}`} state={item}>
-            <p className="text-blue-500">More details</p>
-          </Link>
-        </div>
+        <button
+          className={`py-1 px-2 rounded ${
+            isInCart ? "bg-gray-300 cursor-not-allowed" : "bg-light-pink"
+          }`}
+          onClick={() => handleAddToCart(item)}
+          disabled={isInCart}
+          data-tooltip-id="my-tooltip"
+          data-tooltip-content={isInCart ? "Item already in cart" : null}
+        >
+          <FontAwesomeIcon
+            icon={faShoppingCart}
+            className={`text-white ${isInCart ? "text-gray-500" : ""}`}
+          />
+        </button>
+        <Tooltip
+          id="my-tooltip"
+          style={{
+            backgroundColor: "rgb(2,0,36)",
+            color: "white",
+            zIndex: "999",
+          }}
+        />
       </div>
-    </TooltipProvider>
+      <div className="flex justify-center"></div>
+    </div>
   );
 };
 
