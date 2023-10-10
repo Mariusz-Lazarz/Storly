9a1e71cb31dd4ae2585125ec9b23745c7275587e
Mariusz Łazarz <mariusz.lazarz97@gmail.com>
2023-10-10 21:32:55 +0200

    styling edit products list with grid for better responsivity

--- src/components/UserPanel/ProductItem.js
+++ src/components/UserPanel/ProductItem.js
@@ -3,27 +3,32 @@ import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 function ProductItem({ product, handleEditClick, handleRemoveItem }) {
   return (
-    <div className="flex justify-between items-center">
-      <img
-        src={product.imageLink}
-        alt={product.title}
-        className="w-16 h-16 object-cover"
-      />
-      <div className="flex-1 ml-4 flex items-center justify-between">
-        <span className="text-md">{product.title}</span>
-        <span className="hidden lg:block text-sm">{product.description}</span>
-        <span className="hidden lg:block text-sm">{product.category}</span>
-        <span className="hidden lg:block text-sm">{product.brand}</span>
-        <span className="hidden lg:block text-sm">{product.variant}</span>
-        <span className="hidden lg:block text-sm">
-          Quantity: {product.quantity}
-        </span>
-
-        <span className="hidden lg:block text-sm">${product.price}</span>
+    <div className="grid grid-cols-4 md:grid-cols-7 items-center">
+      <div className="col-span-1">
+        <img
+          src={product.imageLink}
+          alt={product.title}
+          className="w-16 h-16 object-cover"
+        />
       </div>
-      <div className="flex space-x-4">
+      <div className="col-span-1">
+        <span className="block text-md">{product.title}</span>
+      </div>
+      <div className="hidden md:col-span-1 md:block text-sm">
+        {product.category}
+      </div>
+      <div className="hidden md:col-span-1 md:block text-sm">
+        {product.brand}
+      </div>
+      <div className="hidden md:col-span-1 md:block text-sm">
+        {product.variant}
+      </div>
+      <div className="col-span-1">
+        <span className="block text-sm">${product.price}</span>
+      </div>
+      <div className="col-span-1 flex gap-2 justify-center items-center">
         <button
-          className="px-2 py-1 ml-4 bg-blue-500 text-white rounded-lg"
+          className="px-2 py-1 bg-blue-500 text-white rounded-lg"
           onClick={() => handleEditClick(product)}
         >
           <FontAwesomeIcon icon={faEdit} />
