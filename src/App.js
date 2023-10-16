02a8aec4d63ce704ca95918d67c100ec32febb42
Mariusz Łazarz <mariusz.lazarz97@gmail.com>
2023-10-16 16:27:10 +0200

    adding lazy loading components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Header/Navbar";
-import AddItemForm from "./components/AddItem/AddItemForm";
-import NotFound from "./components/Error/NotFound";
-import Store from "./components/Store/Store";
-import HomePage from "./components/Home/HomePage";
 import Footer from "./components/Footer/Footer";
-import "./App.css";
-import Cart from "./components/Cart/Cart";
-import About from "./components/Home/About";
-import Contact from "./components/Home/Contact";
-import UserPanel from "./components/UserPanel/UserPanel";
-import ProductDetails from "./components/Store/ProductDetails";
 import BotChat from "./components/ChatBot/BotChat";
+import "./App.css";
+import LoadingSpinner from "./utils/LoadingSpinner";
+
+const AddItemForm = lazy(() => import("./components/AddItem/AddItemForm"));
+const NotFound = lazy(() => import("./components/Error/NotFound"));
+const Store = lazy(() => import("./components/Store/Store"));
+const HomePage = lazy(() => import("./components/Home/HomePage"));
+const Cart = lazy(() => import("./components/Cart/Cart"));
+const About = lazy(() => import("./components/Home/About"));
+const Contact = lazy(() => import("./components/Home/Contact"));
+const UserPanel = lazy(() => import("./components/UserPanel/UserPanel"));
+const ProductDetails = lazy(() => import("./components/Store/ProductDetails"));
 
 function App() {
   return (
@@ -20,21 +22,22 @@ function App() {
       <div className="App flex flex-col min-h-screen bg-gradient-to-b from-blue-50 via-white to-pink-200">
         <Navbar />
         <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/add" element={<AddItemForm />} />
-            <Route path="/store" element={<Store />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            {/* <Route path="/bot" element={<BotChat />} /> */}
-            <Route path="/userPanel" element={<UserPanel />} />
-            <Route path="/product/:id" element={<ProductDetails />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <Suspense fallback={<LoadingSpinner />}>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/add" element={<AddItemForm />} />
+              <Route path="/store" element={<Store />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/userPanel" element={<UserPanel />} />
+              <Route path="/product/:id" element={<ProductDetails />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
         </main>
         <Footer />
-        <BotChat/>
+        <BotChat />
       </div>
     </Router>
   );
