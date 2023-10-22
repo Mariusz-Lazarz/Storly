d2992ac257f50b8b914bbe4b91d85edfe1c55f43
Mariusz Łazarz <mariusz.lazarz97@gmail.com>
2023-10-22 15:18:35 +0200

    adjusting filter component display

--- src/components/Store/Filter.js
+++ src/components/Store/Filter.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCaretDown, faCaretUp } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faCaretDown, faCaretUp } from "@fortawesome/free-solid-svg-icons";
 import Modal from "../Modal/Modal";
 
 const Filter = ({ isOpen, onClose, setFilters }) => {
-  const [categoryOpen, setCategoryOpen] = useState(false);
-  const [brandOpen, setBrandOpen] = useState(false);
+  // const [categoryOpen, setCategoryOpen] = useState(false);
+  // const [brandOpen, setBrandOpen] = useState(false);
   const [selectedCategories, setSelectedCategories] = useState([]);
   const [selectedBrands, setSelectedBrands] = useState([]);
   const [minPrice, setMinPrice] = useState("");
@@ -51,70 +51,72 @@ const Filter = ({ isOpen, onClose, setFilters }) => {
         <div className="mb-4">
           <div className="flex justify-between items-center mb-2">
             <span>Category</span>
-            <FontAwesomeIcon
+            {/* <FontAwesomeIcon
               icon={categoryOpen ? faCaretUp : faCaretDown}
               onClick={() => setCategoryOpen(!categoryOpen)}
-            />
+            /> */}
           </div>
-          {categoryOpen && (
-            <div className="flex flex-col">
-              {[
-                "Mobile",
-                "Laptop",
-                "Tablet",
-                "Headset",
-                "Earbuds",
-                "Gaming Accessory",
-                "Keyboard",
-              ].map((item) => (
-                <label key={item} className="inline-flex items-center mt-3">
-                  <input
-                    type="checkbox"
-                    className="form-checkbox h-5 w-5 text-gray-600"
-                    checked={selectedCategories.includes(item)}
-                    onChange={() =>
-                      handleCheckboxChange(setSelectedCategories, item)
-                    }
-                  />
-                  <span className="ml-2 text-gray-700">{item}</span>
-                </label>
-              ))}
-            </div>
-          )}
+          {/* {categoryOpen && ( */}
+          <div className="flex flex-col">
+            {[
+              "Mobile",
+              "Laptop",
+              "Tablet",
+              "Headset",
+              "Earbuds",
+              "Gaming Accessory",
+              "Keyboard",
+            ].map((item) => (
+              <label key={item} className="inline-flex items-center mt-3">
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-5 w-5 text-gray-60"
+                  checked={selectedCategories.includes(item)}
+                  onChange={() =>
+                    handleCheckboxChange(setSelectedCategories, item)
+                  }
+                />
+                <span className="ml-2 text-gray-700 dark:text-white">
+                  {item}
+                </span>
+              </label>
+            ))}
+          </div>
+          {/* // )} */}
         </div>
         <div className="mb-4">
           <div className="flex justify-between items-center mb-2">
             <span>Brand</span>
-            <FontAwesomeIcon
+            {/* <FontAwesomeIcon
               icon={brandOpen ? faCaretUp : faCaretDown}
               onClick={() => setBrandOpen(!brandOpen)}
-            />
+            /> */}
           </div>
-          {brandOpen && (
-            <div className="flex flex-col">
-              {[
-                "Samsung",
-                "Apple",
-                "Microsoft",
-                "Razer",
-                "Amazon",
-                "Sony",
-                "Logitech",
-              ].map((item) => (
-                <label key={item} className="inline-flex items-center mt-3">
-                  <input
-                    type="checkbox"
-                    className="form-checkbox h-5 w-5 text-gray-600"
-                    checked={selectedBrands.includes(item)}
-                    onChange={() =>
-                      handleCheckboxChange(setSelectedBrands, item)
-                    }
-                  />
-                  <span className="ml-2 text-gray-700">{item}</span>
-                </label>
-              ))}
-            </div>
-          )}
+          {/* {brandOpen && ( */}
+          <div className="flex flex-col">
+            {[
+              "Samsung",
+              "Apple",
+              "Microsoft",
+              "Razer",
+              "Amazon",
+              "Sony",
+              "Logitech",
+            ].map((item) => (
+              <label key={item} className="inline-flex items-center mt-3">
+                <input
+                  type="checkbox"
+                  className="form-checkbox h-5 w-5 text-gray-600"
+                  checked={selectedBrands.includes(item)}
+                  onChange={() => handleCheckboxChange(setSelectedBrands, item)}
+                />
+                <span className="ml-2 text-gray-700 dark:text-white">
+                  {item}
+                </span>
+              </label>
+            ))}
+          </div>
+          {/* )} */}
         </div>
         <div className="mb-4">
           <div className="flex gap-4">
@@ -123,14 +125,14 @@ const Filter = ({ isOpen, onClose, setFilters }) => {
               placeholder="Min price"
               value={minPrice}
               onChange={(e) => setMinPrice(e.target.value)}
-              className="form-input block w-1/2 p-2 rounded border-gray-300"
+              className="form-input block w-1/2 p-2 rounded border-gray-300 text-black"
             />
             <input
               type="number"
               placeholder="Max price"
               value={maxPrice}
               onChange={(e) => setMaxPrice(e.target.value)}
-              className="form-input block w-1/2 p-2 rounded border-gray-300"
+              className="form-input block w-1/2 p-2 rounded border-gray-300 text-black"
             />
           </div>
         </div>
