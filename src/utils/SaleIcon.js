ac16e04075dcfda1f26a557e214c4c7f155722bf
Mariusz Łazarz <mariusz.lazarz97@gmail.com>
2023-10-26 21:42:05 +0200

    changing size of a sale icon

--- src/utils/SaleIcon.js
+++ src/utils/SaleIcon.js
@@ -2,7 +2,7 @@ const SaleIcon = ({ itemDiscount }) => {
   return (
     <>
       {itemDiscount > 0 && (
-        <div className="absolute h-12 w-12 top-0 right-4">
+        <div className="absolute h-8 w-8 top-0 right-4">
           <img
             src="https://cdn-icons-png.flaticon.com/512/6188/6188700.png"
             alt="sale"
